fe5f8a060d1cf6ff6fc754fadeeaedf989d84493
Hadoop-cpu <Hadoop_CPU@163.com>
2020-07-14 13:40:09 +0800

    第二次提交

---
+++ src/com/qfedu/pojo/User.java
@@ -0,0 +1,12 @@
+package com.qfedu.pojo;
+
+/**
+ * @ClassName: User
+ * @Author: 王赛
+ * @Date: 2020/7/14
+ * @Time: 13:34
+ */
+public class User {
+    private Integer id;
+    private String name;
+}
